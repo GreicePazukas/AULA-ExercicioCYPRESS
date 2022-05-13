e277decd3a518ae0314dbc41ea17a15e1c8d3947
Greice Pazukas <greicemac@hotmail.com>
2022-05-13 17:16:02 +0200

    usando massa de dados

--- cypress/integration/Pre-cadastro.spec.js
+++ cypress/integration/Pre-cadastro.spec.js
@@ -4,11 +4,11 @@
 context('Funcionalidade Pré cadastro', () => {
     
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/my-account/') 
+        cy.visit('my-account') 
     }); 
     
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_email').type('aluno_ebac@teste.com')
         cy.get('#reg_password').type('teste@teste.com')
         cy.get(':nth-child(4) > .button').click()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
-
+const perfil =require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-       cy.visit('http://lojaebac.ebaconline.art.br/my-account/') 
+       cy.visit('my-account') 
     })
         
     afterEach(() => {
@@ -16,7 +16,26 @@ it('Deve fazer login com sucesso', () => {
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
     cy.get('.page-title').should('contain','Minha conta')
+
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+});
+
+it('Deve fazer login com sucesso - Usando arquivo de dado', () => {
+     cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.page-title').should('contain','Minha conta')
+});
+
+it.only('Deve fazer login com sucesso - usando fixture ', () => {
+    cy.fixture('perfil').then(dados =>{
+        cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha,{log:false})
+        cy.get('.woocommerce-form > .button').click()
+    
+        cy.get('.page-title').should('contain','Minha conta')
+    })
 });
 
 it('Deve exibir uma mensagem de erro ao inserir o formato de email incorreto ', () => {
